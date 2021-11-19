bacd92a0dea17cbe201a7887e363372df535ef55
Christopher Speltz <chrisspeltz@msn.com>
2021-11-18 23:06:17 -0600

    created fetch function for spoonacular api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,3 @@
-
-// var getMovies = function () {
-// //    var response = fetch("https://api.themoviedb.org/3/discover/movie?api_key=b2defd411e4c4ccada84680b336db68b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=westerns&with_watch_monetization_types=flatrate");
-//    fetch("https://api.themoviedb.org/3/discover/movie?api_key=b2defd411e4c4ccada84680b336db68b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=westerns&with_watch_monetization_types=flatrate").then(function (response) {
-//        console.log("inside", response);
-//    });
-   
-//    console.log(response);
-// }
-
-// getMovies();
-
-// fetch("https://api.themoviedb.org/3/discover/movie?api_key=b2defd411e4c4ccada84680b336db68b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=westerns&with_watch_monetization_types=flatrate").then(function (response) {
-//        console.log("inside", response);
-//    });
-   
-//    console.log(response);
-
-
-//    fetch("https://api.themoviedb.org/3/discover/movie?api_key=b2defd411e4c4ccada84680b336db68b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=westerns&with_watch_monetization_types=flatrate").then(function(response) {
-//   response.json().then(function(data) {
-//     console.log(data);
-//   });
-// });
-
 var getMovies = function (params) {
     // format tmdb api url
     // var tmdbApiUrl ="https://api.themoviedb.org/3/discover/movie?api_key=b2defd411e4c4ccada84680b336db68b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=" +  year + "&with_genres=" + genre + "&with_watch_monetization_types=free";
@@ -37,3 +12,18 @@ var getMovies = function (params) {
 };
 
 getMovies()
+
+
+var getRecipes = function (params) {
+    // format spoonacular api url
+    var spoonApiUrl ="https://api.spoonacular.com/recipes/complexSearch?apiKey=98f2006279d64b1894e8b2e18d3687b3&query=pasta&maxFat=25&number=2";
+
+    // make a request to the url
+    fetch(spoonApiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log("The Recipe is", data)
+        });
+    });
+};
+
+getRecipes()
