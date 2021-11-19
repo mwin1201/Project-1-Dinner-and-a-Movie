2e4b5364fa6eab088acc70b62eedcfb012133201
Max W <mwinter@wesleyan.edu>
2021-11-19 15:33:55 -0600

    added modals to page

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// initialize the dinner modal
+$(document).ready(function() {
+    $("#dinner-modal").modal();
+});
+
+// initialize the meal type dropdown
+$(document).ready(function(){
+    $('#meal-type').formSelect();
+});
+
+// initialize the cuisine dropdown
+$(document).ready(function(){
+    $('#cuisine').formSelect();
+});
+
+// initialize the diet dropdown
+$(document).ready(function(){
+    $('#diet').formSelect();
+});
+
+// initialize the movie modal
+$(document).ready(function() {
+    $('#movie-modal').modal();
+});
+
+// initialize the sort dropdown
+$(document).ready(function(){
+    $('#sort').formSelect();
+});
