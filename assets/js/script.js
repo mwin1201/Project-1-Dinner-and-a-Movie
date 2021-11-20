dfd3941c0ae5d5b616c82740ceddf59115b97e6d
Christopher Speltz <chrisspeltz@msn.com>
2021-11-20 10:56:42 -0600

    updated js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,33 @@ $(document).ready(function() {
 $(document).ready(function(){
     $('#sort').formSelect();
 });
+
+var getMovies = function (params) {
+    // format tmdb api url
+    // var tmdbApiUrl ="https://api.themoviedb.org/3/discover/movie?api_key=b2defd411e4c4ccada84680b336db68b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=" +  year + "&with_genres=" + genre + "&with_watch_monetization_types=free";
+    var tmdbApiUrl ="https://api.themoviedb.org/3/discover/movie?api_key=b2defd411e4c4ccada84680b336db68b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=westerns&with_watch_monetization_types=flatrate";
+
+    // make a request to the url
+    fetch(tmdbApiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data)
+        });
+    });
+};
+
+getMovies()
+
+
+var getRecipes = function (params) {
+    // format spoonacular api url
+    var spoonApiUrl ="https://api.spoonacular.com/recipes/complexSearch?apiKey=98f2006279d64b1894e8b2e18d3687b3&query=pasta&maxFat=25&number=2";
+
+    // make a request to the url
+    fetch(spoonApiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log("The Recipe is", data)
+        });
+    });
+};
+
+getRecipes()
